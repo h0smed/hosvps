2e3c266bf85ab3903f5db48229b9b5f27be4e48a
h0smed <71381853+h0smed@users.noreply.github.com>
2021-02-07 18:46:07 +0200

    Update liteVPS.sh

--- liteVPS.sh
+++ liteVPS.sh
@@ -109,6 +109,14 @@ printf "$g$b    Installing Obs-studio $endc$enda" >&2
 printf "\r$c$b    OBS-Studio Installed $endc$enda\n" >&2 ||
 printf "\r$r$b    Error Occured $endc$enda\n" >&2
 
+# Install VLC Media Player 
+printf "$g$b    Installing VLC Media Player $endc$enda" >&2
+{
+    sudo apt install vlc -y
+} &> /dev/null &&
+printf "\r$c$b    VLC Media Player Installed $endc$enda\n" >&2 ||
+printf "\r$r$b    Error Occured $endc$enda\n" >&2
+
 # Install OpenShot 
 printf "$g$b    Installing OpenShot $endc$enda" >&2
 {
