89b8706f2fba0749fbebdbdbe2030e4984ff4c5f
h0smed <71381853+h0smed@users.noreply.github.com>
2021-02-27 16:40:33 +0200

    Update liteVPS.sh

--- liteVPS.sh
+++ liteVPS.sh
@@ -73,20 +73,17 @@ printf "\r$c$b    Chrome Remote Desktop Installed $endc$enda\n" >&2 ||
 
 
 
-# Install Desktop Environment (XFCE4)
+# Install Desktop Environment (cinnamon)
 printf "$g$b    Installing Desktop Environment $endc$enda" >&2
 {
-    sudo DEBIAN_FRONTEND=noninteractive \
-        apt install --assume-yes cinnamon-core desktop-base
-    sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/cinnamon-session" > /etc/chrome-remote-desktop-session'  
+    sudo DEBIAN_FRONTEND=noninteractive \	
+        apt install --assume-yes task-cinnamon-desktop
+    sudo bash -c 'echo "exec /usr/bin/cinnamon-session-cinnamon2d" > ~/.chrome-remote-desktop-session'  
     sudo apt install --assume-yes xscreensaver
     sudo systemctl disable lightdm.service
-    sudo apt install --assume-yes task-cinnamon-desktop
-
 } &> /dev/null &&
 printf "\r$c$b    Desktop Environment Installed $endc$enda\n" >&2 ||
 { printf "\r$r$b    Error Occured $endc$enda\n" >&2; exit; }
-
 
 
 # Install Google Chrome
@@ -95,9 +92,40 @@ printf "$g$b    Installing Google Chrome $endc$enda" >&2
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg --install google-chrome-stable_current_amd64.deb
     sudo apt install --assume-yes --fix-broken
+    sudo apt-get install -y xfce4-terminal
 } &> /dev/null &&
 printf "\r$c$b    Google Chrome Installed $endc$enda\n" >&2 ||
 printf "\r$r$b    Error Occured $endc$enda\n" >&2
+
+
+# Install OBS-Studio (Livestream)
+printf "$g$b    Installing Obs-studio $endc$enda" >&2
+{
+    sudo apt install ffmpeg -y
+    sudo add-apt-repository ppa:obsproject/obs-studio -y
+    sudo apt install obs-studio -y
+    sudo apt install --assume-yes --fix-broken
+} &> /dev/null &&
+printf "\r$c$b    OBS-Studio Installed $endc$enda\n" >&2 ||
+printf "\r$r$b    Error Occured $endc$enda\n" >&2
+
+# Install VLC Media Player 
+printf "$g$b    Installing VLC Media Player $endc$enda" >&2
+{
+    sudo apt install vlc -y
+} &> /dev/null &&
+printf "\r$c$b    VLC Media Player Installed $endc$enda\n" >&2 ||
+printf "\r$r$b    Error Occured $endc$enda\n" >&2
+
+# Install OpenShot & Kdenlive
+printf "$g$b    Installing OpenShot & Kdenlive $endc$enda" >&2
+{
+    sudo apt install openshot -y
+    sudo apt install kdenlive -y
+} &> /dev/null &&
+printf "\r$c$b    OpenShot & Kdenlive Installed $endc$enda\n" >&2 ||
+printf "\r$r$b    Error Occured $endc$enda\n" >&2
+
 
 
 
