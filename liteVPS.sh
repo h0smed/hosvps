8f6221ab89a8ec355998ab11c9158a48d3a2defe
h0smed <71381853+h0smed@users.noreply.github.com>
2021-02-28 14:33:17 +0200

    Update liteVPS.sh

--- liteVPS.sh
+++ liteVPS.sh
@@ -77,8 +77,8 @@ printf "\r$c$b    Chrome Remote Desktop Installed $endc$enda\n" >&2 ||
 printf "$g$b    Installing Desktop Environment $endc$enda" >&2
 {
        sudo DEBIAN_FRONTEND=noninteractive \
-        apt install --assume-yes xfce4 desktop-base
-    sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+        apt install --assume-yes lubuntu-desktop
+    sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/lubuntu-session" > /etc/chrome-remote-desktop-session'  
     sudo apt install --assume-yes xscreensaver
     sudo apt install xfce4-terminal
     sudo systemctl disable lightdm.service
