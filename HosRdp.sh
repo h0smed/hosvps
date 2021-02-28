1f64a10761ae2c58726f6d1cc2f384bdbfff5fe9
h0smed <71381853+h0smed@users.noreply.github.com>
2021-02-28 02:59:59 +0200

    Update HosRdp.sh

--- HosRdp.sh
+++ HosRdp.sh
@@ -144,14 +144,12 @@ printf "\r$c$b    VLC Media Player Installed $endc$enda\n" >&2 ||
 printf "\r$r$b    Error Occured $endc$enda\n" >&2
 
 # Install OpenShot 
-printf "$g$b    Installing OpenShot Kdenlive $endc$enda" >&2
+printf "$g$b    Installing Kdenlive $endc$enda" >&2
 {
-    sudo add-apt-repository ppa:openshot.developers/ppa
     sudo apt-get update
-    sudo apt-get install openshot-qt
     sudo apt install kdenlive -y
 } &> /dev/null &&
-printf "\r$c$b    OpenShot Kdenlive Installed $endc$enda\n" >&2 ||
+printf "\r$c$b    Kdenlive Installed $endc$enda\n" >&2 ||
 printf "\r$r$b    Error Occured $endc$enda\n" >&2
 
 
