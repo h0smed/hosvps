06a24d8911b5cec41ccccf8c6c712994a8537f95
h0smed <71381853+h0smed@users.noreply.github.com>
2021-04-09 21:34:06 +0200

    Update Gtest.sh

--- Gtest.sh
+++ Gtest.sh
@@ -10,8 +10,9 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-apt install --assume-yes mate-desktop-environment
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/mate-session" > /etc/chrome-remote-desktop-session'  
+apt install --assume-yes ubuntu-gnome-desktop
+sudo add-apt-repository ppa:gnome3-team/gnome3
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/gnome-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
