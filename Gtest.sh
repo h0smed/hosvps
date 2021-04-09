42cf93fe02afe912a715555cf2145ade7dea1dfb
h0smed <71381853+h0smed@users.noreply.github.com>
2021-04-09 21:08:17 +0200

    Update Gtest.sh

--- Gtest.sh
+++ Gtest.sh
@@ -1,42 +1,117 @@
 #! /bin/bash
-printf "Installing RDP Please Wait 4 or 5 Min It's Made by Hassan Ahmed 2021" >&2
-{
-sudo useradd -m HosRDP
-sudo adduser HosRDP sudo
-echo 'HosRDP:123456' | sudo chpasswd
+
+# Make Instance Ready for Remote Desktop or RDP
+
+b='\033[1m'
+r='\E[31m'
+g='\E[32m'
+c='\E[36m'
+endc='\E[0m'
+enda='\033[0m'
+
+clear
+
+# Branding
+
+printf """$c$b
+ 
+██╗  ██╗ █████╗ ███████╗███████╗ █████╗ ███╗   ██╗     █████╗ ██╗  ██╗███╗   ███╗███████╗██████╗ 
+██║  ██║██╔══██╗██╔════╝██╔════╝██╔══██╗████╗  ██║    ██╔══██╗██║  ██║████╗ ████║██╔════╝██╔══██╗
+███████║███████║███████╗███████╗███████║██╔██╗ ██║    ███████║███████║██╔████╔██║█████╗  ██║  ██║
+██╔══██║██╔══██║╚════██║╚════██║██╔══██║██║╚██╗██║    ██╔══██║██╔══██║██║╚██╔╝██║██╔══╝  ██║  ██║
+██║  ██║██║  ██║███████║███████║██║  ██║██║ ╚████║    ██║  ██║██║  ██║██║ ╚═╝ ██║███████╗██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝    ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝╚═════╝ 
+                                                                                                 
+    $r  By Hassan Ahmed © 2021 $c 
+          
+$endc$enda""";
+
+
+
+# Used Two if else type statements, one is simple second is complex. So, don't get confused or fear by seeing complex if else statement '^^.
+
+# Creation of user
+printf "\n\nCreating user " >&2
+if sudo useradd -m HosRdp &> /dev/null
+then
+  printf "\ruser created $endc$enda\n" >&2
+  printf "\rUsername:- HosRdp $endc$enda\n" >&2
+  printf "\rPassword:- 123456 $endc$enda\n" >&2
+else
+  printf "\r$r$b Error Occured $endc$enda\n" >&2
+  exit
+fi
+
+# Add user to sudo group
+sudo adduser HosRdp sudo
+
+# Set password of user to 'root'
+echo 'HosRdp:123456' | sudo chpasswd
+
+# Change default shell from sh to bash
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
-sudo apt-get update
-wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
-sudo dpkg --install chrome-remote-desktop_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
+
+# Initialisation of Installer
+printf "\n\n$c$b    Loading Installer $endc$enda" >&2
+if sudo apt-get update &> /dev/null
+then
+    printf "\r$g$b    Installer update Loaded $endc$enda\n" >&2
+else
+    printf "\r$r$b    Error Occured $endc$enda\n" >&2
+    exit
+fi
+
+# Installing Chrome Remote Desktop
+printf "\n$g$b    Installing Chrome Remote Desktop $endc$enda" >&2
+{
+    wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+    sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+    sudo apt install --assume-yes --fix-broken
+} &> /dev/null &&
+printf "\r$c$b    Chrome Remote Desktop Installed $endc$enda\n" >&2 ||
+{ printf "\r$r$b    Error Occured $endc$enda\n" >&2; exit; }
+
+
+
+# Install Desktop Environment (cinnamon
+printf "$g$b    Installing Desktop Environment $endc$enda" >&2
+{
 sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes ubuntu-gnome-desktop
 sudo add-apt-repository ppa:gnome3-team/gnome3
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/gnome-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
-sudo systemctl disable lightdm.service
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg --install google-chrome-stable_current_amd64.deb
-sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano -y 
-sudo adduser HosRDP chrome-remote-desktop
-sudo apt install ffmpeg -y
-sudo add-apt-repository ppa:obsproject/obs-studio -y
-sudo apt install obs-studio -y
-sudo apt install vlc -y
-sudo apt install firefox -y
-sudo apt install xarchiver -y
-sudo apt install openshot -y
+    sudo apt install xfce4-terminal
+    sudo systemctl disable lightdm.service
 } &> /dev/null &&
-printf "\nSetup Complete " >&2 ||
-printf "\nError Occured " >&2
-printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
-read -p "Paste Here:- " CRP
-su - HosRDP -c """$CRP"""
-printf 'Check Now The RDP in https://remotedesktop.google.com/access/ \n\n'
-if sudo apt-get upgrade &> /dev/null
-then
-    printf "\n\nUpgrade Completed " >&2
-else
-    printf "\n\nError Occured " >&2
-fi
+printf "\r$c$b    Desktop Environment Installed $endc$enda\n" >&2 ||
+{ printf "\r$r$b    Error Occured $endc$enda\n" >&2; exit; }
+
+
+# Install Google Chrome
+printf "$g$b    Installing Google Chrome $endc$enda" >&2
+{
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    sudo dpkg --install google-chrome-stable_current_amd64.deb
+    sudo apt install --assume-yes --fix-broken
+    sudo apt-get install -y xfce4-terminal
+} &> /dev/null &&
+printf "\r$c$b    Google Chrome Installed $endc$enda\n" >&2 ||
+printf "\r$r$b    Error Occured $endc$enda\n" >&2
+
+
+printf "\n$g$b    Installation RDP Completed Successfully $endc$enda\n\n" >&2
+
+
+
+# Adding user to CRP group
+sudo adduser HosRdp chrome-remote-desktop
+
+# Finishing Work
+printf '\nVisit http://remotedesktop.google.com/headless and Copy the command after authentication\n'
+read -p "Paste Command Here:- " CRP
+su - HosRdp -c """$CRP"""
+
+printf "\n$c$b if mistakenly wrote wrong command or pin, Rerun the current box or run command 'su - HosRdp -c '<CRP Command Here>' $endc$enda\n" >&2
+printf "\n$c$b https://remotedesktop.google.com/access to access your RDP, do not close browser tab to keep colab running ' $endc$enda\n" >&2
+printf "\n$g$b Finished Succesfully ,Enjoy it $endc$enda"
